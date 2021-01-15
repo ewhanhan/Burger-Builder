b96acb0096f356a7dfd4073eb592cc072665dc09
Ewhan Han <ewhan.han@gmail.com>
2021-01-15 10:20:10 -0500

    Fix empty ingredients logic

--- src/component/Burger/Burger.js
+++ src/component/Burger/Burger.js
@@ -3,7 +3,7 @@ import BurgerIngredients from './BurgerIngredients/BurgerIngredients.js';
 import styles from './Burger.module.css';
 
 const Burger = (props) => {
-  const ingredientsItems = Object.keys(props.ingredients)
+  let ingredientsItems = Object.keys(props.ingredients)
     .map((ingredient) => {
       return [...Array(props.ingredients[ingredient])].map((item, index) => {
         return <BurgerIngredients key={ingredient + index} type={ingredient} />;
@@ -14,7 +14,7 @@ const Burger = (props) => {
     }, []);
 
   if (ingredientsItems.length === 0) {
-    <p>There are no ingredients inside this burger</p>;
+    ingredientsItems = <p>There are no ingredients inside this burger</p>;
   }
 
   return (
