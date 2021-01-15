43a20d90bc8af1af19c27ba01ef991538061edff
Ewhan Han <ewhan.han@gmail.com>
2021-01-15 09:56:35 -0500

    Render state ingredients to component

--- src/component/Burger/Burger.js
+++ src/component/Burger/Burger.js
@@ -3,9 +3,16 @@ import BurgerIngredients from './BurgerIngredients/BurgerIngredients.js';
 import styles from './Burger.module.css';
 
 const Burger = (props) => {
+  const ingredientsItems = Object.keys(props.ingredients).map((ingredient) => {
+    return [...Array(props.ingredients[ingredient])].map((item, index) => {
+      return <BurgerIngredients key={ingredient + index} type={ingredient} />;
+    });
+  });
+
   return (
     <div className={styles.Burger}>
       <BurgerIngredients type="bread-top" />
+      {ingredientsItems}
       <BurgerIngredients type="bread-bottom" />
     </div>
   );
