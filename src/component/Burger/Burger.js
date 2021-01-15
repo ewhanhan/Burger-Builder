8c2f0618afd3376eca97c6243192b6b48c15cf47
Ewhan Han <ewhan.han@gmail.com>
2021-01-15 09:59:46 -0500

    Added logic to handle empty ingredients

--- src/component/Burger/Burger.js
+++ src/component/Burger/Burger.js
@@ -3,11 +3,19 @@ import BurgerIngredients from './BurgerIngredients/BurgerIngredients.js';
 import styles from './Burger.module.css';
 
 const Burger = (props) => {
-  const ingredientsItems = Object.keys(props.ingredients).map((ingredient) => {
-    return [...Array(props.ingredients[ingredient])].map((item, index) => {
-      return <BurgerIngredients key={ingredient + index} type={ingredient} />;
-    });
-  });
+  const ingredientsItems = Object.keys(props.ingredients)
+    .map((ingredient) => {
+      return [...Array(props.ingredients[ingredient])].map((item, index) => {
+        return <BurgerIngredients key={ingredient + index} type={ingredient} />;
+      });
+    })
+    .reduce((arr, elem) => {
+      return arr.concat(elem);
+    }, []);
+
+  if (ingredientsItems.length === 0) {
+    <p>There are no ingredients inside this burger</p>;
+  }
 
   return (
     <div className={styles.Burger}>
